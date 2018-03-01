77fab006fa8b2b334253768d914e504be1952f88
Chelsea Hartman <overstreetchelsea@gmail.com>
2018-03-01 18:52:35 -0500

    Desktop Footer

--- wp-content/themes/nail-union/footer.php
+++ wp-content/themes/nail-union/footer.php
@@ -1,10 +1,10 @@
 <footer class="footer" role="contentinfo">
-	<div class="footer-holder">
-		<div class="footer-left">
+	<div class="footer-holder" data-equalizer="footer" data-equalize-on="medium">
+		<div class="footer-left" data-equalizer-watch="footer">
 			<?php the_field('address', 'option') ?>
 		</div>
 
-		<div class="footer-right">
+		<div class="footer-right" data-equalizer-watch="footer">
 			<div class="social"><a href="<?php the_field('facebook_link', 'option') ?>" class="facebook-link"><i class="fa fa-facebook-f" aria-hidden="true"></i></a></div>
 			<div class="important-links">
 				<?php joints_footer_links(); ?>
