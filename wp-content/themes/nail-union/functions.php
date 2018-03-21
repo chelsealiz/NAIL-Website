1b8c3544645eb9fd346ca54e7207613c21003d88
Chelsea Hartman <overstreetchelsea@gmail.com>
2018-03-20 23:25:36 -0400

    Literally no one needs to be using the admin bar on the front of the site

--- wp-content/themes/nail-union/functions.php
+++ wp-content/themes/nail-union/functions.php
@@ -1,30 +1,30 @@
 <?php
 // Theme support options
-require_once(get_template_directory().'/assets/functions/theme-support.php'); 
+require_once(get_template_directory().'/assets/functions/theme-support.php');
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/assets/functions/cleanup.php'); 
+require_once(get_template_directory().'/assets/functions/cleanup.php');
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/assets/functions/enqueue-scripts.php'); 
+require_once(get_template_directory().'/assets/functions/enqueue-scripts.php');
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/assets/functions/menu.php'); 
+require_once(get_template_directory().'/assets/functions/menu.php');
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/assets/functions/sidebar.php'); 
+require_once(get_template_directory().'/assets/functions/sidebar.php');
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/assets/functions/comments.php'); 
+require_once(get_template_directory().'/assets/functions/comments.php');
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/assets/functions/page-navi.php'); 
+require_once(get_template_directory().'/assets/functions/page-navi.php');
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/assets/translation/translation.php'); 
+require_once(get_template_directory().'/assets/translation/translation.php');
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/assets/functions/related-posts.php'); 
+// require_once(get_template_directory().'/assets/functions/related-posts.php');
 
 
 
@@ -32,73 +32,100 @@ require_once(get_template_directory().'/assets/translation/translation.php');
 
 // HIDE CUSTOM FIELDS WHEN CONSTANT IS NOT SET OR IS FALSE
 if(!defined('WP_LOCAL') || !WP_LOCAL){
-    add_filter('acf/settings/show_admin', '__return_false');
+	add_filter('acf/settings/show_admin', '__return_false');
 }
 // SAVE ACF TO FILES
 add_filter('acf/settings/save_json', function() {
-    return get_stylesheet_directory() . '/custom-acf-json';
-});
+		return get_stylesheet_directory() . '/custom-acf-json';
+	});
 add_filter('acf/settings/load_json', function($paths) {
-    $paths[] = get_stylesheet_directory() . '/custom-acf-json';
-    return $paths;
-});
+		$paths[] = get_stylesheet_directory() . '/custom-acf-json';
+		return $paths;
+	});
 
-require_once(get_template_directory().'/assets/functions/Mobile_Detect.php'); 
+require_once(get_template_directory().'/assets/functions/Mobile_Detect.php');
 function body_class_device_detect( $classes ) {
 	$detect = new Mobile_Detect;
-    if ( $detect->isMobile() ) {
-        $classes[] = 'device-mobile';
-    }
-    if ( $detect->isTablet() ) {
-        $classes[] = 'device-tablet';
-    }
-    if (!$detect->isMobile() && !$detect->isTablet()){
-        $classes[] = 'device-desktop';
-    }
-    return $classes;
+	if ( $detect->isMobile() ) {
+		$classes[] = 'device-mobile';
+	}
+	if ( $detect->isTablet() ) {
+		$classes[] = 'device-tablet';
+	}
+	if (!$detect->isMobile() && !$detect->isTablet()){
+		$classes[] = 'device-desktop';
+	}
+	return $classes;
 }
 add_filter( 'body_class', 'body_class_device_detect' );
 
 
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
-		'page_title' 	=> 'Site Settings',
-		'menu_title'	=> 'Site Settings',
-		'menu_slug' 	=> 'site-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
-	));
+			'page_title' => 'Site Settings',
+			'menu_title' => 'Site Settings',
+			'menu_slug' => 'site-settings',
+			'capability' => 'edit_posts',
+			'redirect' => false
+		));
 }
 
 
 function display_acf_button_object($button, $link_text = null) {
-    $url = '#';
-    $target = '';
+	$url = '#';
+	$target = '';
 
-    if(is_null($link_text) && isset($button['link_text']) && !empty($button['link_text'])) {
-        $link_text = $button['link_text'];
-    } else if(is_null($link_text)) {
-        $link_text = "Learn More";
-    }
+	if(is_null($link_text) && isset($button['link_text']) && !empty($button['link_text'])) {
+		$link_text = $button['link_text'];
+	} else if(is_null($link_text)) {
+		$link_text = "Learn More";
+	}
 
-    $class = isset($button['button_style']) ? $button['button_style'] : 'primary';
-    switch($button['button_type']){
-        case 'internal':
-            $url = $button['button_post'];
-            break;
-        case 'email':
-            $url = 'mailto:'.$button['email_address'];
-            break;
-        case 'external':
-            $url = $button['external_url'];
-            $target = 'target="_blank"';
-            break;
-        case 'file':
-            $url = $button['file_download'];
-            $target = 'target="_blank"';
-            break;
-    }
-?>
-    <a href="<?php echo $url; ?>" <?php echo $target; ?> class="button <?php echo $class; ?>"><?php echo $link_text; ?></a>
-<?php
+	$class = isset($button['button_style']) ? $button['button_style'] : 'primary';
+	switch($button['button_type']){
+		case 'internal':
+		$url = $button['button_post'];
+		break;
+		case 'email':
+		$url = 'mailto:'.$button['email_address'];
+		break;
+		case 'external':
+		$url = $button['external_url'];
+		$target = 'target="_blank"';
+		break;
+		case 'file':
+		$url = $button['file_download'];
+		$target = 'target="_blank"';
+		break;
+	}
+	?>
+	<a href="<?php echo $url; ?>" <?php echo $target; ?> class="button <?php echo $class; ?>"><?php echo $link_text; ?></a>
+	<?php
 }
+
+
+// Disable the user admin bar on public side on registration
+add_action('user_register','trash_public_admin_bar');
+
+function trash_public_admin_bar($user_ID) {
+	update_user_meta( $user_ID, 'show_admin_bar_front', 'false' );
+}
+
+//Removes the 'Show Toolbar' option.
+function as_remove_personal_options( $subject ) {
+	$subject = preg_replace( '#<tr class="show-admin-bar">.+?/tr>#s', '', $subject, 1 );
+		return $subject;
+	}
+
+	function as_profile_subject_start() {
+		ob_start( 'as_remove_personal_options' );
+	}
+
+	function as_profile_subject_end() {
+		ob_end_flush();
+	}
+	add_action( 'admin_head-profile.php', 'as_profile_subject_start' );
+	add_action( 'admin_footer-profile.php', 'as_profile_subject_end' );
+
+	add_action( 'admin_head-user-edit.php', 'as_profile_subject_start' );
+	add_action( 'admin_footer-user-edit.php', 'as_profile_subject_end' );
