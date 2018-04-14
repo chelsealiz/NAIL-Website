a254e747ee8057af246c65c98442ea92129f3196
Chelsea Hartman <overstreetchelsea@gmail.com>
2018-04-13 21:09:16 -0400

    Mobile Page Builder Styling

--- wp-content/themes/nail-union/parts/pagebuilder.php
+++ wp-content/themes/nail-union/parts/pagebuilder.php
@@ -19,46 +19,66 @@
 
 		<?php while( have_rows('page_builder') ): the_row(); ?>
 
-			<?php if ( get_row_layout('wysiwyg') ): ?>
+			<?php if ( get_row_layout() == 'wysiwyg'): ?>
 				<div class="pb-section pb-wysiwyg">
 					<div class="text-holder">
 						<?php the_sub_field('wysiwyg'); ?>
-						</div>
+					</div>
 				</div>
 
-				<?php elseif( get_row_layout('full_width_video') ) ?>
+			<?php elseif( get_row_layout() == 'full_width_video' ): ?>
 				<div class="pb-section pb-fw-vid">
-
+					<div class="responsive-embed widescreen">
+						<?php the_sub_field('embed_code'); ?>
+					</div>
 				</div>
 
-				<?php elseif( get_row_layout('image_left') ) ?>
+			<?php elseif( get_row_layout() == 'image_left' ): ?>
 				<div class="pb-section pb-img-left">
-
+					<?php $image = get_sub_field('image'); ?>
+					<div class="img-l" style="background-image: url(<?php echo $image['url']; ?>);"></div>
+					<div class="text-r">
+						<?php the_sub_field('text_section'); ?>
+					</div>
 				</div>
 
-				<?php elseif( get_row_layout('three_column') ) ?>
+			<?php elseif( get_row_layout() == 'three_column' ): ?>
 				<div class="pb-section pb-three-col">
+					<?php if( have_rows('columns') ): ?>
+						<?php while ( have_rows('columns') ) : the_row(); ?>
+							<div class="col-section">
+								<img src="<?php the_sub_field('image'); ?>">
+									<?php the_sub_field('text'); ?>
+								</div>
+							<?php endwhile; ?>
 
-				</div>
+						<?php endif; ?>
+					</div>
 
-				<?php elseif( get_row_layout('full_width_image') ) ?>
-				<div class="pb-section pb-fw-img">
+				<?php elseif( get_row_layout() == 'full_width_image' ): ?>
+					<?php $image = get_sub_field('image'); ?>
+					<div class="pb-section pb-fw-img" style="background-image: url(<?php echo $image['url']; ?>);">
 
-				</div>
+					</div>
 
-				<?php elseif( get_row_layout('full_width_quote') ) ?>
-				<div class="pb-section pb-fw-quote">
+				<?php elseif( get_row_layout() == 'full_width_quote' ): ?>
+					<div class="pb-section pb-fw-quote">
+						<h2 class="quote"><?php the_sub_field('quote_text'); ?></h2>
+						<?php if (get_sub_field('attribution')): ?>
+							<h3 class='attribution'><?php the_sub_field('attribution'); ?></h3>
+						<?php endif; ?>
 
-				</div>
+					</div>
 
-				<?php elseif( get_row_layout('two_column_text') ) ?>
-				<div class="pb-section pb-two-col">
+				<?php elseif( get_row_layout() == 'two_column_text' ): ?>
+					<div class="pb-section pb-two-col">
+						<div class="text-l"><?php the_sub_field('text_left'); ?></div>
+						<div class="text-r"><?php the_sub_field('text_right'); ?></div>
+					</div>
+				<?php endif; ?>
 
-				</div>
-			<?php endif; ?>
-
-		<?php endwhile; ?>
-	</div>
+			<?php endwhile; ?>
+		</div>
 
 
-<?php endif; ?>
+	<?php endif; ?>
